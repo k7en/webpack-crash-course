aec9cf2df4cce2539fefde39ae7e392200514f7c
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-11-06 18:27:15 +0900

    export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash'
+import {Nijou} from './utilities'
 
+ 
 function component() {
     const element = document.createElement('div')
     const array = ['Hello', 'webpack', '!']
     element.innerHTML = _.join(array,' ')
+    console.log(Nijou(3))
     return element;
 }
 
